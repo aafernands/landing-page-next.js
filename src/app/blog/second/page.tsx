e9fa861f94a501c3928d7ae6c25362948d1411a7
Alex Fernandes <62030201+aafernands@users.noreply.github.com>
2025-10-21 19:10:24 -0400

    Initial commit

---
+++ src/app/blog/second/page.tsx
@@ -0,0 +1,8 @@
+export  default function SecondtBlogPost() {
+    return (
+        <main>
+        <h1>Second Blog Post</h1>
+        <p>This is the content of the second blog post.</p>
+        </main>
+    );
+}
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,8 @@
+export default function Profile() {
+    return (
+        <main>
+        <h1>User Profile</h1>
+        <p>Welcome to your profile page!</p>
+        </main>
+    );
+}
